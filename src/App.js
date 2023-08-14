0be078db157f43af183ee30f8b43a686d3db95a9
Honmary66 <honeymarymathew2004@gmail.com>
2023-08-14 23:10:24 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to workshop about github
+          This is a demo project for a workshop.
         </p>
         <a
           className="App-link"
