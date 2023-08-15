0043c364e6f651440e1d62dba7c65abcd2fab34e
aelan-anna-mathew <141669485+aelan-anna-mathew@users.noreply.github.com>
2023-08-15 23:07:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is a demo project for a workshop.
+          This change is done by aelan.
         </p>
         <a
           className="App-link"
