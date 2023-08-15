e7434b1f49deea1d3089c10fef8a5821bddd7463
Honmary66 <honeymarymathew2004@gmail.com>
2023-08-15 22:17:58 +0530

    deleted unwanted files

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is a demo project.
+          This is a demo project for a workshop on github.
         </p>
         <a
           className="App-link"
